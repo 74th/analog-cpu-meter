071493e449ad61a22869baffdd97ac792f9c99b6
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-06-15 14:06:21 +0900

    2IO

--- esp32-c3-server/src/main.cpp
+++ esp32-c3-server/src/main.cpp
@@ -8,8 +8,12 @@ const char *ssid = WIFI_SSID;
 const char *pass = WIFI_PASSWORD;
 WebServer server(80);
 
-#define METER_PIN GPIO_NUM_0
-#define METER_PWM_CH 0
+#define METER_PIN_1 GPIO_NUM_0
+#define METER_PIN_2 GPIO_NUM_1
+#define METER_PWM_CH_1 0
+#define METER_PWM_CH_2 1
+
+#define LOG_INTERVAL_MS 2000
 
 void handleRoot(void)
 {
@@ -21,14 +25,25 @@ void handleNotFound(void)
     server.send(404, "application/json", "{\"message\": \"Not Found.\"}");
 }
 
-void handleCPU(void)
+void setPWMValue(uint8_t pwm_ch, uint8_t value)
+{
+    // 3.3Vを上限とすると、若干低くなったため、3.1Vとして計算
+    float_t analogValue = 256 * 3 / 3.1 * value / 100;
+    uint16_t pwmValue = (uint16_t)analogValue;
+
+    Serial.printf("pwm value: %d\n", pwmValue);
+
+    ledcWrite(pwm_ch, analogValue);
+}
+
+void handleSingle(const char *path, uint8_t pwm_ch)
 {
     if (server.method() != HTTP_POST)
     {
         server.send(405, "application/json", "{\"message\": \"Method Not Allowed.\"}");
         return;
     }
-    Serial.println("POST /cpu");
+    Serial.printf("POST /%s\r\n", path);
 
     String requestBody = server.arg("plain");
     Serial.printf("request body: %s\r\n", requestBody.c_str());
@@ -39,7 +54,7 @@ void handleCPU(void)
     if (error)
     {
         Serial.println("failed to deserialize");
-        Serial.println("POST /cpu 400");
+        Serial.printf("POST /%s 400\r\n", path);
         server.send(400, "application/json", "{\"message\": \"Bad Request.\"}");
         return;
     }
@@ -48,22 +63,76 @@ void handleCPU(void)
 
     if (value < 0 || 100 < value)
     {
-        Serial.println("POST /cpu 400");
+        Serial.printf("POST /%s 400\r\n", path);
         server.send(400, "application/json", "{\"message\": \"Bad Request. Unexpected value.\"}");
         return;
     }
 
     Serial.printf("input value: %d\n", value);
 
-    // 3.3Vを上限とすると、若干低くなったため、3.1Vとして計算
-    float_t analogValue = 256 * 3 / 3.1 * value / 100;
-    uint16_t pwmValue = (uint16_t)analogValue;
+    setPWMValue(pwm_ch, value);
 
-    Serial.printf("pwm value: %d\n", pwmValue);
+    Serial.printf("POST /%s 200\r\n", path);
+    server.send(200, "text/json", "{\"message\": \"Success.\"}");
+}
 
-    ledcWrite(METER_PWM_CH, analogValue);
+void handleCPU(void)
+{
+    handleSingle("cpu", METER_PWM_CH_1);
+}
 
-    Serial.println("POST /cpu 200");
+void handleMEM(void)
+{
+    handleSingle("mem", METER_PWM_CH_2);
+}
+
+void handleValues()
+{
+    if (server.method() != HTTP_POST)
+    {
+        server.send(405, "application/json", "{\"message\": \"Method Not Allowed.\"}");
+        return;
+    }
+    Serial.printf("POST /values\r\n");
+
+    String requestBody = server.arg("plain");
+    Serial.printf("request body: %s\r\n", requestBody.c_str());
+
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, requestBody);
+
+    if (error)
+    {
+        Serial.println("failed to deserialize");
+        Serial.printf("POST /values 400\r\n");
+        server.send(400, "application/json", "{\"message\": \"Bad Request.\"}");
+        return;
+    }
+
+    int32_t cpuValue = doc["cpu"].as<int32_t>();
+
+    if (cpuValue < 0 || 100 < cpuValue)
+    {
+        Serial.printf("POST /values 400\r\n");
+        server.send(400, "application/json", "{\"message\": \"Bad Request. Unexpected value.\"}");
+        return;
+    }
+
+    int32_t memValue = doc["mem"].as<int32_t>();
+
+    if (cpuValue < 0 || 100 < memValue)
+    {
+        Serial.printf("POST /values 400\r\n");
+        server.send(400, "application/json", "{\"message\": \"Bad Request. Unexpected value.\"}");
+        return;
+    }
+
+    Serial.printf("input value: cpu:%d mem:%d\n", cpuValue, memValue);
+
+    setPWMValue(METER_PWM_CH_1, cpuValue);
+    setPWMValue(METER_PWM_CH_2, memValue);
+
+    Serial.printf("POST /values 200\r\n");
     server.send(200, "text/json", "{\"message\": \"Success.\"}");
 }
 
@@ -72,10 +141,14 @@ void setup()
     Serial.begin(9600);
 
     // PWMの設定
-    pinMode(METER_PIN, OUTPUT);
-    ledcSetup(METER_PWM_CH, 1000, 8);
-    ledcAttachPin(METER_PIN, METER_PWM_CH);
-    ledcWrite(METER_PWM_CH, 0);
+    pinMode(METER_PIN_1, OUTPUT);
+    ledcSetup(METER_PWM_CH_1, 1000, 8);
+    ledcAttachPin(METER_PIN_1, METER_PWM_CH_1);
+    ledcWrite(METER_PWM_CH_1, 0);
+    pinMode(METER_PIN_2, OUTPUT);
+    ledcSetup(METER_PWM_CH_2, 1000, 8);
+    ledcAttachPin(METER_PIN_2, METER_PWM_CH_2);
+    ledcWrite(METER_PWM_CH_2, 0);
 
     // WiFiの接続
     WiFi.begin(ssid, pass);
@@ -89,11 +162,25 @@ void setup()
     // Webサーバの開始
     server.on("/", handleRoot);
     server.on("/cpu", HTTP_POST, handleCPU);
+    server.on("/mem", HTTP_POST, handleMEM);
+    server.on("/values", HTTP_POST, handleValues);
     server.onNotFound(handleNotFound);
     server.begin();
 }
 
+unsigned long lastLogTimeMS = 0;
+
 void loop()
 {
+    unsigned long now = millis();
+
     server.handleClient();
+
+    if (now > lastLogTimeMS + LOG_INTERVAL_MS)
+    {
+        lastLogTimeMS = now;
+
+        Serial.printf("[%12d]IP:%s \r\n", now, WiFi.localIP());
+        Serial.printf("[%12d]CPU: %d, MEM: %d\r\n", now, ledcRead(METER_PWM_CH_1), ledcRead(METER_PWM_CH_2));
+    }
 }
--- esp32-c3-server/test.py
+++ esp32-c3-server/test.py
@@ -1,13 +1,14 @@
+import sys
 import time
 import json
 from urllib import request
 from urllib import error
 
-API_HOST = "http://192.168.1.102"
+API_HOST = "http://192.168.1.105"
 
 
-def putValue(value: int):
-    api = "/cpu"
+def putValue(path: str, value: int):
+    api = f"/{path}"
     request_url = API_HOST + api
 
     data = {"value": value}
@@ -29,15 +30,50 @@ def putValue(value: int):
     except error.URLError as e:
         print("通信失敗")
 
+def putValues(cpu: int, mem: int):
+    request_url = API_HOST + "/values"
 
-def main():
+    data = {"cpu": cpu, "mem": mem}
+
+    header = {"Content-Type": "application/json"}
+
+    try:
+        req_body = json.dumps(data)
+        print("POST", request_url)
+        print("REQ:", req_body)
+        request_post = request.Request(
+            url=request_url, headers=header, method="POST", data=req_body.encode()
+        )
+        with request.urlopen(request_post) as res:
+            res_body = res.read().decode()
+        print("RES:", res_body)
+    except error.HTTPError as e:
+        print(f"{e.code} RES:", e.read().decode())
+    except error.URLError as e:
+        print("通信失敗")
+
+def main1():
     while True:
         values = [0, 10, 50, 75, 100]
         for value in values:
-            print(f"put: {value}")
-            putValue(value)
+            print(f"put CPU: {value}")
+            putValue("cpu", value)
+            time.sleep(0.5)
+            print(f"put MEM: {value}")
+            putValue("mem", value)
+            time.sleep(0.5)
+
+def main2():
+    while True:
+        values = [0, 10, 50, 75, 100]
+        for n,_ in enumerate(values):
+            print(f"put CPU: {values[n]} MEM: {values[n-1]}")
+            putValues(values[n], values[n-1])
             time.sleep(0.5)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == "1":
+        main1()
+    else:
+        main2()
